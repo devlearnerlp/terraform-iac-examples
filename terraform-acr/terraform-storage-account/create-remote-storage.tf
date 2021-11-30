a183e5ea40b4186acd07a788386feb6ab16a608b
kpraghav <kpraghav@gmail.com>
2021-11-30 21:41:44 +0530

    updated provider

--- terraform-acr/terraform-storage-account/create-remote-storage.tf
+++ terraform-acr/terraform-storage-account/create-remote-storage.tf
@@ -1,14 +1,14 @@
+provider "azurerm" {
+  features {}
+}
+
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = "2.39.0"
     }
   }
-}
-
-provider "azurerm" {
-  features {}
 }
 
 resource "random_string" "resource_code" {
